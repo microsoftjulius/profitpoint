31c60bbc7b8ba0bd5b7dc975487f8ae68a60f63a
microsoftjulius <julisema4@gmail.com>
2020-11-21 02:47:23 -0500

    Solved the error for the amount on the cards

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,25 +31,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if(auth()->user()->currency == "/="){
-            $total_earnings         = $this->earnings_instance->getMyTotalEarnings() * $this->dollar_rates_instance->getDollarRate();
-            $user_total_withdraws   = $this->earnings_instance->getMyTotalWithDraws()  * $this->dollar_rates_instance->getDollarRate();
-            $user_total_balance     = $this->earnings_instance->getMyTotalBalance();
-            $user_total_investments = $this->investments_instance->calculateTotalInvestmentsMadeByLoggedInUser()  * $this->dollar_rates_instance->getDollarRate();
-            $today_investment       = $this->investments_instance->getTodaysInvestmentsForLoggedinUser() * $this->dollar_rates_instance->getDollarRate();
-            $monthly_investment     = $this->investments_instance->getThisMonthsInvestmentsForLoggedinUser() * $this->dollar_rates_instance->getDollarRate();
-            $todays_withdraws       = $this->earnings_instance->getWithdrawsMadeByLoggedinUserToday()  * $this->dollar_rates_instance->getDollarRate();
-            $months_withdraws       = $this->earnings_instance->getWithdrawsMadeByLoggedinUserThisMonth() * $this->dollar_rates_instance->getDollarRate();
-            $todays_earnings        = $this->earnings_instance->getTodaysEarnings() * $this->dollar_rates_instance->getDollarRate();
-            $this_months_earnings   = $this->earnings_instance->getThisMonthsEarnings() * $this->dollar_rates_instance->getDollarRate();
-            $todays_balance         = $this->earnings_instance->getLoggedInUsersTodaysEarnings() * $this->dollar_rates_instance->getDollarRate();
-            $months_balance         = $this->earnings_instance->getLoggedInUsersMonthsEarnings() * $this->dollar_rates_instance->getDollarRate();
-            $total_account_balance_to_admin = $this->getTotalAccountBalance() * $this->dollar_rates_instance->getDollarRate();
-            $total_investments_to_admin = $this->getTotalInvestments() * $this->dollar_rates_instance->getDollarRate();
-            $total_withdraws = $this->getTotalWithdraws() * $this->dollar_rates_instance->getDollarRate();
-            $transactions    = $this->getTransactionsOverView();
-            $over_all_earnings = $this->earnings_instance->getTotalEarnings() * $this->dollar_rates_instance->getDollarRate();
-        }else{
             $total_earnings         = $this->earnings_instance->getMyTotalEarnings();
             $user_total_withdraws   = $this->earnings_instance->getMyTotalWithDraws();
             $user_total_balance     = $this->earnings_instance->getMyTotalBalance();
@@ -67,11 +48,9 @@ class HomeController extends Controller
             $total_withdraws = $this->getTotalWithdraws();
             $transactions    = $this->getTransactionsOverView();   
             $over_all_earnings = $this->earnings_instance->getTotalEarnings();
-        }
-        $dollar_rate = $this->dollar_rates_instance->getDollarRate();
         return view('welcome',compact('total_earnings','user_total_withdraws','user_total_balance','user_total_investments',
             'today_investment','monthly_investment','todays_withdraws','months_withdraws','todays_earnings','this_months_earnings',
-            'todays_balance','months_balance','total_account_balance_to_admin','total_investments_to_admin','total_withdraws','transactions','over_all_earnings','dollar_rate'));
+            'todays_balance','months_balance','total_account_balance_to_admin','total_investments_to_admin','total_withdraws','transactions','over_all_earnings'));
     }
 
     //this function gets the transactions collection and shows it to the admin
