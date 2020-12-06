e6ba9fd4182ee117304003843d59a6c54f7331a7
microsoftjulius <julisema4@gmail.com>
2020-12-06 01:20:28 -0500

    Kenyan numbers

--- app/Http/Controllers/ValidNumbersController.php
+++ app/Http/Controllers/ValidNumbersController.php
@@ -7,7 +7,8 @@ use Str;
 
 class ValidNumbersController extends Controller
 {
-    static protected $valid_contacts = ['25677','25678','25670','25679','25671','25675','25675','25620','25639','25641'];
+    static protected $valid_contacts = ['25677','25678','25670','25679','25671','25675','25675','25620','25639','25641',
+                                        '25477','25478','25470','25479','25471','25475','25475','25420','25439','25441','25479'];
     static protected $to_append_valid_contacts = ['077','078','070','079','071','075','075','020','039','041'];
     /**
      * Check if the number has 13 figures. if so, 
@@ -34,11 +35,12 @@ class ValidNumbersController extends Controller
             if(!ctype_digit($contact_number)){
                 return response()->json("The number you entered is wrong, Valid numbers don't contain Alphabetical Letters and special characters");
             }
-            if(Str::substr($contact_number, 0, 4) == '+256'){
+            if(Str::substr($contact_number, 0, 4) == '+256' || Str::substr($contact_number, 0, 4) == '+254'){
                 if(in_array(Str::substr($contact_number, 1, 5), self::$valid_contacts)){
                     return $contact_number;
                 }else{
-                    return response()->json("The supplied contact is not valid, valid contacts start with these digits 25677, 25678, 25670, 25679, 25671, 25675, 25675, 25620, 25639, 25641");
+                    return response()->json("The supplied contact is not valid, valid contacts start with these digits 25677, 25678, 25670, 25679, 25671, 25675, 25675, 25620, 25639, 25641,
+                    25477, 25478, 25470, 25479, 25471, 25475, 25475, 25420, 25439, 25441, 25479");
                 }
             }else{
                 return response()->json("The contact number you entered is not valid, the number must start with +256");
@@ -80,7 +82,8 @@ class ValidNumbersController extends Controller
                 return "+256".Str::substr($contact_number, 1, 9);
             }else{
                 return response()->json("The supplied contact is not valid, 
-                    valid contacts start with these digits 077, 078, 070, 079, 071, 075, 075, 020, 039, 041 or 25677, 25678, 25670, 25679, 25671, 25675, 25675, 25620, 25639, 25641");
+                    valid contacts start with these digits 077, 078, 070, 079, 071, 075, 075, 020, 039, 041 or 25677, 25678, 25670, 25679, 25671, 25675, 25675, 25620, 25639, 25641,
+                    25477, 25478, 25470, 25479, 25471, 25475, 25475, 25420, 25439, 25441, 25479. if you aint in uganda, consider adding the country code to your number");
             }
         }else{
             return response()->json("The supplied number is not valid, Kindly check the length of the number to verify");
